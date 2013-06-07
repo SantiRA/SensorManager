65f618ff5364fd176a49b4b02d306558efc8c513
ECB4 <ecb4@alumnes.udl.cat>
2013-06-07 18:35:05 +0200

    Last tested version of SensorsManager

--- SensorsManager/src/com/example/sensorsmanager/InputListener.java
+++ SensorsManager/src/com/example/sensorsmanager/InputListener.java
@@ -3,124 +3,157 @@ package com.example.sensorsmanager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.view.View;
 import android.widget.TextView;
 
-public class InputListener implements SensorEventListener, View.OnClickListener {
+public class InputListener implements SensorEventListener {
 
-	float x,y,z, xtemp, ytemp, ztemp;
-	float tx, ty, tz;
-	float xx, yy, zz;
-	float accuracy,show;
-	float codx,cody,codz;
-	int codx2,cody2,codz2;
-	int min=0;
+	public float[] previousValues;
+	public float[] sensorLectures;
+	public int[] codifiedValues;
+
+	public float offsetX, offsetY, offsetZ;
+	public float accuracy;
+	int min = 0;
 	int max = 255;
-	
+
 	public TextView labelAccelerometer;
 	public TextView labelAccuracy;
 	public TextView labelCod;
-	int ultim_enviat = 10, valor_mov = 10;
-	boolean enviar = true;
 
-	
-	public InputListener() {	
-		x = y = z = 0f;
-		codx = cody = codz = 0;
-		xtemp = ytemp = ztemp = 0f;
-		xx = yy = zz = 0f;
+	public InputListener() {
+		accuracy = 0.0f;
+		previousValues = new float[3];
+		sensorLectures = new float[3];
+		codifiedValues = new int[3];
+		for (int axisIndex = 0; axisIndex < 3; axisIndex++) {
+			previousValues[axisIndex] = 0;
+			sensorLectures[axisIndex] = 0;
+			codifiedValues[axisIndex] = 0;
+		}
+
+		offsetX = offsetY = offsetZ = 0f;
 		accuracy = 0f;
-		show =0f;
+	}
 
+	public void processValues(float[] imputValues) {
+		float temporalEventValues[] = imputValues;
+		float calibratedEventValues[];
+
+		sensorLectures = imputValues;
+
+		calibratedEventValues = applyOffsetToImputValues(temporalEventValues);
+		if (significativeChanges(calibratedEventValues)) {
+			codifiedValues = CodificateAndUpdateValues(calibratedEventValues);
+		}
+
+		updateInformation(calibratedEventValues);
 	}
-	
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		
+
+	public float[] applyOffsetToImputValues(float[] imputValuesToCalibrate) {
+
+		float[] calibratedImputValues = new float[3];
+		calibratedImputValues[0] = imputValuesToCalibrate[0] + offsetX;
+		calibratedImputValues[1] = imputValuesToCalibrate[1] + offsetY;
+		calibratedImputValues[2] = imputValuesToCalibrate[2] + offsetZ;
+
+		return calibratedImputValues;
 	}
-	
+
+	public boolean significativeChanges(float[] calibratedEventValues) {
+		for (int axisIndex = 0; axisIndex < 3; axisIndex++) {
+			if (calibratedEventValues[axisIndex] > previousValues[axisIndex]
+					+ accuracy
+					|| calibratedEventValues[axisIndex] < previousValues[axisIndex]
+							- accuracy) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int[] CodificateAndUpdateValues(float[] calibratedEventValues) {
+
+		int temporalCodifiedValue;
+		int[] temporalCodifiedValues = new int[3];
+
+		for (int axisIndex = 0; axisIndex < 3; axisIndex++) {
+			if (calibratedEventValues[axisIndex] != 0) {
+				temporalCodifiedValue = (int) ((calibratedEventValues[axisIndex] + 10) / 0.078125);
+
+				if (temporalCodifiedValue < min)
+					temporalCodifiedValues[axisIndex] = min;
+
+				else if (temporalCodifiedValue > max)
+					temporalCodifiedValues[axisIndex] = max;
+
+				else
+					temporalCodifiedValues[axisIndex] = temporalCodifiedValue;
+			} else
+				temporalCodifiedValues[axisIndex] = 128;
+		}
+
+		return temporalCodifiedValues;
+	}
+
+	public void updateInformation(float[] calibratedEventValues) {
+		if (labelAccelerometer != null) {
+			labelAccelerometer.setText("X: " + calibratedEventValues[0]
+					+ "\nY: " + calibratedEventValues[1] + "\nZ: "
+					+ calibratedEventValues[2]);
+		}
+
+		if (labelAccuracy != null) {
+			float showPercentageAccuracy = (float) (100 - ((100 * accuracy) / 10.5));
+			if (accuracy == 0) {
+				showPercentageAccuracy = 0;
+			}
+			labelAccuracy.setText("Accuracy: " + showPercentageAccuracy
+					+ "%, corrector value = " + accuracy);
+		}
+
+		if (labelCod != null) {
+			labelCod.setText("Codification: X: " + codifiedValues[0] + ", Y: "
+					+ codifiedValues[1] + ", Z: " + codifiedValues[2]);
+		}
+	}
+
 	public void onSensorChanged(SensorEvent event) {
-		if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			tx = event.values[0];
-			ty = event.values[1];
-			tz = event.values[2];
-			xtemp = tx + xx;
-			ytemp = ty + yy; 
-			ztemp = tz + zz;
-			if((xtemp > x+accuracy || xtemp < x-accuracy) || (ytemp > y+accuracy || ytemp < y-accuracy) || (ztemp > z+accuracy || ztemp < z-accuracy)){
-				x = xtemp;
-				y = ytemp;
-				z = ztemp;
-				codx = x;
-				cody = y;
-				codz = z;
-				
-				if (codx != 0){
-					codx2 = (int) ((codx+10)/0.078125);
-					if (codx2 < min)
-						codx2 = min;
-					if (codx2 > max)
-						codx2 = max;
-				}
-				else 
-					codx2 = 128;
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 
-				
-				
-				if (cody != 0){
-					cody2 = (int) ((cody+10)/0.078125);
-					if (cody2 < min)
-						cody2 = min;
-					if (cody2 > max)
-						cody2 = max;
-				}
-				else 
-					cody2 = 128;
-				
-				
-				if (codz != 0){
-					codz2 = (int) ((codz+10)/0.078125);
-					if (codz2 < min)
-						codz2 = min;
-					if (codz2 > max)
-						codz2 = max;
-				}
-				else 
-					codz2 = 128;			
-			}	
+			sensorLectures = event.values;
+			processValues(event.values);
 		}
-		if(labelAccelerometer != null)
-			labelAccelerometer.setText("X: " + x + "\nY: " + y + "\nZ: " + z);
-		if(labelAccuracy != null)
-			if(accuracy == 0){
-				labelAccuracy.setText("Accuracy: 100%, corrector value = 0");
-			}
-			else{
-				show = (float) (100-(100*accuracy)/10.5);
-				labelAccuracy.setText("Accuracy: " + show + "%, corrector value = " + accuracy);
-			}
-		if(labelCod != null){
-			labelCod.setText("Codification: X: " + codx2 + ", Y: " + cody2 + ", Z: " + codz2);
-		}
-	}			
-	
-	
-	public void onClick(View v) {
-		
-		switch(v.getId())
-		{
-			case R.id.buttonSetBaseValues:  
-				xx = -tx;
-				yy = -ty;
-				zz = -tz;
-			break;
-			case R.id.buttonAccuracyLess:
-				accuracy += 0.01f; 
+	}
+
+	public float modifyAccuracy(String action) {
+		if (action.equals("IncreaseAccuracy")) {
+			accuracy -= 0.01f;
+			accuracy = Math.max(accuracy, 0.0f);
+
+		} else {
+			if (action.equals("ReduceAccuracy")) {
+				accuracy += 0.01f;
 				accuracy = Math.min(accuracy, 10.5f);
-			break;
-			case R.id.buttonAccuracyPlus:
-				accuracy -= 0.01f;
-				accuracy = Math.max(accuracy, 0f);
-			break;		
-		}		
+
+			}
+		}
+		return accuracy;
+	}
+
+	public float[] setBaseValues() {
+		float[] testResult = new float[3];
+		if (sensorLectures != null) {
+			offsetX = -sensorLectures[0];
+			offsetY = -sensorLectures[1];
+			offsetZ = -sensorLectures[2];
+			testResult[0] = offsetX;
+			testResult[1] = offsetY;
+			testResult[2] = offsetZ;
+		}
+		return testResult;
+	}
+
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
 	}
 }
--- SensorsManager/src/com/example/sensorsmanager/MainActivity.java
+++ SensorsManager/src/com/example/sensorsmanager/MainActivity.java
@@ -4,74 +4,109 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	TextView label1, label2, label3;
+
+	TextView textViewAccuracy, textViewImputValues, textViewCodified;
 	Button button, plus, less, stop;
 	Sensor accelerometer;
 	SensorManager sensorManager;
-	InputListener il;	
+	InputListener inputListenerInstance;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		textViewAccuracy = (TextView) findViewById(R.id.label1);
+		textViewImputValues = (TextView) findViewById(R.id.label2);
+		textViewCodified = (TextView) findViewById(R.id.label3);
+
+		// Create input listener
+		inputListenerInstance = new InputListener();
+		inputListenerInstance.labelAccelerometer = textViewAccuracy;
+		inputListenerInstance.labelAccuracy = textViewImputValues;
+		inputListenerInstance.labelCod = textViewCodified;
+
+		// Set button action
+		setButtonAction();
+
+		// Set up accelerometer
+		createAccelerometer();
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// Set sensor events
+		sensorManager.registerListener(inputListenerInstance, accelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sensorManager.unregisterListener(inputListenerInstance);
+	}
+
+	private void setButtonAction() {
+		button = (Button) findViewById(R.id.buttonSetBaseValues);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (inputListenerInstance != null) {
+					inputListenerInstance.setBaseValues();
+				}
+			}
+		});
+
+		plus = (Button) findViewById(R.id.buttonAccuracyPlus);
+		plus.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(inputListenerInstance != null){
+					inputListenerInstance.modifyAccuracy("IncreaseAccuracy");
+				}
+			}
+		});
+
+		less = (Button) findViewById(R.id.buttonAccuracyLess);
+		less.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(inputListenerInstance != null){
+					inputListenerInstance.modifyAccuracy("ReduceAccuracy");
+				}
+			}
+		});
+	}
+
+	private void createAccelerometer() {
+		// Start sensors
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		accelerometer = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	}
 	
-	int valor;
 	
-   @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
- 
-        label1 = (TextView)findViewById(R.id.label1);   
-        label2 = (TextView)findViewById(R.id.label2);
-        label3 = (TextView)findViewById(R.id.label3);
-        
-        //	Create input listener
-        il = new InputListener();
-        il.labelAccelerometer = label1;
-        il.labelAccuracy = label2;
-        il.labelCod = label3;
-        
-        //	Set button action
-        setButtonAction();
-        
-        //	Set up accelerometer
-        createAccelerometer();        
-    }
-   
-   @Override
-   public void onStart() {
-	   super.onStart();
-   }
-   
-   @Override
-   protected void onResume() {
-	   super.onResume();
-	   //Set sensor events
-       sensorManager.registerListener( il, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-   }
-   
-   @Override
-   protected void onPause() {
-	   super.onPause();
-	   sensorManager.unregisterListener(il);
-   }
-   
-   private void setButtonAction() {
-	   button = (Button) findViewById(R.id.buttonSetBaseValues);
-	   button.setOnClickListener(il);
-	   
-	   plus = (Button) findViewById(R.id.buttonAccuracyPlus);
-	   plus.setOnClickListener(il);
-	   
-	   less = (Button) findViewById(R.id.buttonAccuracyLess);
-	   less.setOnClickListener(il);	   
-   }
-   
-      
-   private void createAccelerometer() {
-	   //Start sensors
-       sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-       accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-   }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 }
+
